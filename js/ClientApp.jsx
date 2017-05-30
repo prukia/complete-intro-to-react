cd54d0d454a5da7833a1f474a84ad4cd978dd8b3
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-05-30 12:02:22 -0500

    add babel and lint and webpages

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,36 @@
-const ce = React.createElement;
+import React from 'react';
+// same as const React= require (x)
+// x = 'react'
+import {render} from 'react-dom';
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
+// to replace React.createElement
+const ce = React.createElement
+// you wouldn't call it something other than props
+  const MyTitle = function(props){
+    return (
+      ce('div', null,
+      ce('h1', {style: {color: props.color}}, props.title);
+    )
+    )
+  };
+// creating a react component (usable)
+// MyFirstComponent is a class..it's rendering a div
+// a component is incapsulated
+  const MyFirstComponent = function() {
+    // instead of null you can pass an empty object {}
+    // inside of the div you are nesting
+    return ce(
     'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
-};
+    {id: 'my-first-component'},
+    ce(MyTitle, {title: 'Game of Thrones', color: 'YellowGreen'}),
+    ce(MyTitle, {title: 'Stranger Things', color: 'GreenYellow'}),
+    ce(MyTitle, {title: 'Rick and Morty', color: 'LimeGreen'}),
+    ce(MyTitle, {title: 'Silicon Valley', color: 'peru'})
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+    // ce('h1', null, 'This is my first component!')
+  );
+  };
+  render(
+    ce(MyFirstComponent),
+    document.getElementById('app')
+    );
