25c4af66e833beb4a3e6a2907452b24f72bb0d55
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-05-30 15:44:36 -0500

    deleted show from Showcard and use {...show} in search

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { string } from 'prop-types'
 
 const ShowCard = props => (
   <div className="show-card">
-    <img alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
+    <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
     <div>
-      <h3>{props.show.title}</h3>
-      <h4>({props.show.year})</h4>
-      <p>{props.show.description}</p>
+      <h3>{props.title}</h3>
+      <h4>({props.year})</h4>
+      <p>{props.description}</p>
     </div>
   </div>
 );
 
+
+//run-time check to see if you are including the write properties
+//shape means object.
+//if you do not put isRequired it means its optional
+ShowCard.propTypes = {
+
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+};
 export default ShowCard;
