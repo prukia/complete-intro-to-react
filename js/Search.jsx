4db443c8f72a2dc6c8a351697718e3f33f1ba0ba
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-05-30 16:38:20 -0500

    added react:state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 // const Search = () => <h1>hi lol this is search</h1>;
 /* prints out actual JSON */
 /* <pre><code>{}</code>{{JSON.stringify(preload, null, 4)}}</pre> */
 
-const Search = () => (
+//converted to ES6 class
+class Search extends Component{
+  // constructor (props){
+  //   super(props)
+  //   //initializing state
+  //   this.state = {
+  //     searchTerm: 'this is some sort of debug statement'
+  //   };
+  //   //second way of doing it
+  //   // this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  //   }
+
+  state = {
+    searchTerm: ''
+  };
+    //way to let react know 'hey, um updating this you need to render'!
+    handleSearchTermChange = (event) => {
+      this.setState({searchTerm: event.target.value})
+  };
+render () {
+  return(
   <div className="search">
+    <header>
+      <h1>svideo</h1>
+      <input onChange= {this.handleSearchTermChange}
+        // onChange= {this.handleSearchTermChange,bind()}
+        //the bad-way of doing it
+         value={this.state.searchTerm}
+         type="text"
+         placeholder="search"/>
+    </header>
     <div>
         {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
     </div>
   </div>
 );
-
+}
+}
 export default Search;
+
+
+//state//
+// a component can have its own state...only a component can modify it's own state
