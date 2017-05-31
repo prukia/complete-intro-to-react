b78a8f8b61e9ffee6129a177198e6cff2c0f609b
github_prukia <rukia.sm.23@gmail.com>
2017-05-31 14:46:43 -0500

    spinner working with rating API

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,16 +1,38 @@
 // @flow
-
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import Spinner from './Spinner';
 
-const Details = (props: { show: Show }) => {
-  const { title, description, year, poster, trailer } = props.show;
+class Details extends Component{
+  state = {
+    apiData: { imdbRating: ''}
+  };
+  componentDidMount (){
+    axios
+    .get(`http://localhost:3000/${this.props.show.imdbID}`)
+    .then((response: { data: { rating: string } }) => {
+      this.setState({ apiData: response.data });
+    });
+  };
+  props: {
+    show: Show
+};
+render(){
+  const { title, description, year, poster, trailer } = this.props.show;
+  let ratingComponent;
+    if(this.state.apiData.rating){
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+} else {
+  ratingComponent = <Spinner />;
+}
   return (
     <div className="details">
       <Header />
       <section>
         <h1>{title}</h1>
         <h2>({year})</h2>
+        {ratingComponent}
         <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
         <p>{description}</p>
       </section>
@@ -24,6 +46,6 @@ const Details = (props: { show: Show }) => {
       </div>
     </div>
   );
+}
 };
-
 export default Details;
