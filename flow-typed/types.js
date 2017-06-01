643433040fca594e2cbbeea103be02cfbb7524b4
github_prukia <rukia.sm.23@gmail.com>
2017-06-01 10:52:53 -0500

    completed async redux

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -6,7 +6,8 @@ export type Show = {
   year: string,
   imdbID: string,
   trailer: string,
-  poster: string
+  poster: string,
+  rating?: string
 };
 
 declare var module: {
@@ -15,7 +16,7 @@ declare var module: {
   }
 };
 //an ActionType can be one of these strings.
-declare type ActionType = 'SET_SEARCH_TERM'
+declare type ActionType = 'SET_SEARCH_TERM' | 'ADD_API_DATA';
 // A & P are generic action types
 // < is like parameters you're giving
 declare type ActionT<A: ActionType, P> = {|
@@ -23,4 +24,4 @@ declare type ActionT<A: ActionType, P> = {|
   payload: P
 |};
 
-export type Action = ActionT<'SET_SEARCH_TERM', string>;
+export type Action = ActionT<'SET_SEARCH_TERM', string> | ActionT<'ADD_API_DATA', Show>;
--- js/reducers.js
+++ js/reducers.js
@@ -1,6 +1,6 @@
 // @flow
 import { combineReducers } from 'redux';
-import { SET_SEARCH_TERM } from './actions';
+import { SET_SEARCH_TERM, ADD_API_DATA } from './actions';
 
 // new way of writing reducers...single action reducers
 const searchTerm = (state='', action: Action) => {
@@ -10,7 +10,15 @@ const searchTerm = (state='', action: Action) => {
   return state;
 };
 
-const rootReducer = combineReducers({ searchTerm });
+const apiData = (state = {}, action: Action) =>{
+  if (action.type === ADD_API_DATA) {
+    return Object.assign({}, state, { [action.payload.imdbID]: action.payload
+     })
+  }
+  return state;
+};
+
+const rootReducer = combineReducers({ searchTerm, apiData });
 // ({ searchTerm }) is the same as ({ searchTerm: searchTerm })
 
 export default rootReducer;
